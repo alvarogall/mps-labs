bda11bf90b58a47d2d668103fc1d732dda623882
alvarogall <varosg@proton.me>
2025-04-09 14:28:02 +0200

    Merged

--- mps-lab3/src/test/java/org/mps/ronqi2/RonQI2SilverTest.java
+++ mps-lab3/src/test/java/org/mps/ronqi2/RonQI2SilverTest.java
@@ -21,15 +21,207 @@ import org.mps.dispositivo.Dispositivo;
 import org.mps.dispositivo.DispositivoSilver;
 
 public class RonQI2SilverTest {
-    /*
-     * Un reconectar, comprueba si el dispositivo desconectado, en ese caso, conecta ambos y devuelve true si ambos han sido conectados. 
-     * Genera las pruebas que estimes oportunas para comprobar su correcto funcionamiento. 
-     * Centrate en probar si todo va bien, o si no, y si se llama a los métodos que deben ser llamados.
-     */
+    
+    @DisplayName("Probar el método reconectar")
+    @Nested
+    public class ReconectarTest {
+       RonQI2 ronQI2;
+       DispositivoSilver dispositivoSilverMock;
 
+       @DisplayName("Se inicializa ronQI2 para cada test")
+       @BeforeEach
+       public void startUp() {
+           //Arrange
+           ronQI2 = new RonQI2Silver();
+           dispositivoSilverMock = mock(DispositivoSilver.class);
+       }
 
+       @DisplayName("El método reconectar devuelve false si el dispositivo está conectado")
+       @Test
+       public void reconectar_DispositivoConectado_ReturnsFalse() {
+           //Arrange
+           when(dispositivoSilverMock.estaConectado()).thenReturn(true);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
 
+           //Act
+           boolean resultado = ronQI2.reconectar();
 
+           //Assert
+           assertFalse(resultado);
+       }
+
+       @DisplayName("El método reconectar devuelve false si el dispositivo está desconectado, no se conecta el sensor de presión y sí se conecta el sensor de sonido")
+       @Test
+       public void reconectar_DispositivoConectado_NoConectaSensorPresion_ConectaSensorSonido() {
+           //Arrange
+           when(dispositivoSilverMock.estaConectado()).thenReturn(false);
+           when(dispositivoSilverMock.conectarSensorPresion()).thenReturn(false);
+           when(dispositivoSilverMock.conectarSensorSonido()).thenReturn(true);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           //Act
+           boolean resultado = ronQI2.reconectar();
+
+           //Assert
+           assertFalse(resultado);
+       }
+
+       @DisplayName("El método reconectar devuelve false si el dispositivo está desconectado, sí se conecta el sensor de presión y no se conecta el sensor de sonido")
+       @Test
+       public void reconectar_DispositivoConectado_ConectaSensorPresion_NoConectaSensorSonido() {
+           //Arrange
+           when(dispositivoSilverMock.estaConectado()).thenReturn(false);
+           when(dispositivoSilverMock.conectarSensorPresion()).thenReturn(true);
+           when(dispositivoSilverMock.conectarSensorSonido()).thenReturn(false);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           //Act
+           boolean resultado = ronQI2.reconectar();
+
+           //Assert
+           assertFalse(resultado);
+       }
+
+       @DisplayName("El método reconectar devuelve false si el dispositivo está desconectado, no se conecta el sensor de presión y no se conecta el sensor de sonido")
+       @Test
+       public void reconectar_DispositivoConectado_NoConectaSensorPresion_NoConectaSensorSonido() {
+           //Arrange
+           when(dispositivoSilverMock.estaConectado()).thenReturn(false);
+           when(dispositivoSilverMock.conectarSensorPresion()).thenReturn(false);
+           when(dispositivoSilverMock.conectarSensorSonido()).thenReturn(false);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           //Act
+           boolean resultado = ronQI2.reconectar();
+
+           //Assert
+           assertFalse(resultado);
+       }
+
+       @DisplayName("El método reconectar devuelve true si el dispositivo está desconectado, sí se conecta el sensor de presión y sí se conecta el sensor de sonido")
+       @Test
+       public void reconectar_DispositivoConectado_ConectaSensorPresion_ConectaSensorSonido() {
+           //Arrange
+           when(dispositivoSilverMock.estaConectado()).thenReturn(false);
+           when(dispositivoSilverMock.conectarSensorPresion()).thenReturn(true);
+           when(dispositivoSilverMock.conectarSensorSonido()).thenReturn(true);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           //Act
+           boolean resultado = ronQI2.reconectar();
+
+           //Assert
+           assertTrue(resultado);
+           verify(dispositivoSilverMock, times(1)).conectarSensorPresion();
+           verify(dispositivoSilverMock, times(1)).conectarSensorSonido();
+       }
+    }
+    
+    @DisplayName("Probar el método obtenerNuevaLectura")
+    @Nested
+    public class ObtenerNuevaLecturaTest {
+       RonQI2 ronQI2;
+       DispositivoSilver dispositivoSilverMock;
+
+       @DisplayName("Se inicializa ronQI2 para cada test")
+       @BeforeEach
+       public void startUp() {
+           //Arrange
+           ronQI2 = new RonQI2Silver();
+           dispositivoSilverMock = mock(DispositivoSilver.class);
+       }
+
+       @DisplayName("El método obtenerNuevaLectura lee una vez los sensores si se le llama una vez")
+       @Test
+       public void obtenerNuevaLectura_UnaLecturaSensores_LLamaUnaVezACadaLeerSensor() {
+           //Arrange
+           when(dispositivoSilverMock.leerSensorPresion()).thenReturn(15.0f);
+           when(dispositivoSilverMock.leerSensorSonido()).thenReturn(10.0f);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           //Act
+           ronQI2.obtenerNuevaLectura();
+
+           //Assert
+           verify(dispositivoSilverMock, times(1)).leerSensorPresion();
+           verify(dispositivoSilverMock, times(1)).leerSensorSonido();
+       }
+       
+       @DisplayName("El método obtenerNuevaLectura lee los sensores cinco veces si se le llama cinco veces")
+       @Test
+       public void obtenerNuevaLectura_5LecturasSensores_LLama5VecesACadaLeerSensor() {
+           //Arrange
+           when(dispositivoSilverMock.leerSensorPresion()).thenReturn(15.0f);
+           when(dispositivoSilverMock.leerSensorSonido()).thenReturn(10.0f);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           //Act
+           for(int i = 0; i < 5; ++i) ronQI2.obtenerNuevaLectura();
+
+           //Assert
+           verify(dispositivoSilverMock, times(5)).leerSensorPresion();
+           verify(dispositivoSilverMock, times(5)).leerSensorSonido();
+       }
+       
+       @DisplayName("El método obtenerNuevaLectura llama más veces al método (7) que el número de lecturas que se pueden almacenar (5) por lo que lee 7 veces los dos sensores")
+       @Test
+       public void obtenerNuevaLectura_LLamaMasVecesNumeroLecturasMax_Llama7VecesACadaLeerSensor() {
+           //Arrange
+           when(dispositivoSilverMock.leerSensorPresion()).thenReturn(15.0f);
+           when(dispositivoSilverMock.leerSensorSonido()).thenReturn(10.0f);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           //Act
+           for(int i = 0; i < 7; ++i) ronQI2.obtenerNuevaLectura();
+
+           //Assert
+           verify(dispositivoSilverMock, times(7)).leerSensorPresion();
+           verify(dispositivoSilverMock, times(7)).leerSensorSonido();
+       }
+    }
+
+    @DisplayName("Probar el método estaConectado")
+    @Nested
+    public class EstaConectadoTest {
+       RonQI2 ronQI2;
+       DispositivoSilver dispositivoSilverMock;
+
+       @DisplayName("Se inicializa ronQI2 para cada test")
+       @BeforeEach
+       public void startUp() {
+           //Arrange
+           ronQI2 = new RonQI2Silver();
+           dispositivoSilverMock = mock(DispositivoSilver.class);
+       }
+
+       @DisplayName("El método estaConectado devuelve true si el dispositivo está conectado")
+       @Test
+       public void estaConectado_DispositivoNoConectado_DevuelveTrue() {
+           // Arrange
+           when(dispositivoSilverMock.estaConectado()).thenReturn(true);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           // Act
+           boolean conectado = ronQI2.estaConectado();
+
+           // Assert
+           assertTrue(conectado);
+       }
+
+       @DisplayName("El método estaConectado devuelve true si el dispositivo no está conectado")
+       @Test
+       public void estaConectado_DispositivoNoConectado_DevuelveFalse() {
+           // Arrange
+           when(dispositivoSilverMock.estaConectado()).thenReturn(false);
+           ronQI2.anyadirDispositivo(dispositivoSilverMock);
+
+           // Act
+           boolean conectado = ronQI2.estaConectado();
+
+           // Assert
+           assertFalse(conectado);
+       }
+    }
 
     @DisplayName("Probar el método inicializar")
     @Nested
@@ -139,7 +331,7 @@ public class RonQI2SilverTest {
 
     @DisplayName("Probar el método evaluarApneaSuenyo")
     @Nested
-    public class evaluarApneaSuenyo {
+    public class EvaluarApneaSuenyoTest {
         private RonQI2 ronquidos;
 
         @DisplayName("Se inicializa RonQI2Silver")
@@ -152,16 +344,4 @@ public class RonQI2SilverTest {
 
 
     }
-    
-    /*
-     * El método evaluarApneaSuenyo, evalua las últimas 5 lecturas realizadas con obtenerNuevaLectura(), 
-     * y si ambos sensores superan o son iguales a sus umbrales, que son thresholdP = 20.0f y thresholdS = 30.0f;, 
-     * se considera que hay una apnea en proceso. Si hay menos de 5 lecturas también debería realizar la media.
-     * /
-     
-     /* Realiza un primer test para ver que funciona bien independientemente del número de lecturas.
-     * Usa el ParameterizedTest para realizar un número de lecturas previas a calcular si hay apnea o no (por ejemplo 4, 5 y 10 lecturas).
-     * https://junit.org/junit5/docs/current/user-guide/index.html#writing-tests-parameterized-tests
-     */
-
 }
