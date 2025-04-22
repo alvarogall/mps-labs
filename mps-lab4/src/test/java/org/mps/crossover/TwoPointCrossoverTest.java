01c7b9713b0a838021b81679d956ff5ea6428930
Zemag_17 <tabletpablo26@gmail.com>
2025-04-22 21:51:43 +0200

    Test Crossover terminado

--- mps-lab4/src/test/java/org/mps/crossover/TwoPointCrossoverTest.java
+++ mps-lab4/src/test/java/org/mps/crossover/TwoPointCrossoverTest.java
@@ -1,10 +1,129 @@
 package org.mps.crossover;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mps.EvolutionaryAlgorithmException;
+import org.mps.mutation.GaussianMutation;
+
 /**
  * @author Pablo Gámez Guerrero
  * @author Álvaro Gallardo Rubio
  */
 
 public class TwoPointCrossoverTest {
-    
+    @DisplayName("Probar que el constructor se crea correctamente")
+    @Test
+    public void twoPointCrossover_FuncionaCorrectamente() {
+        //Arrange
+        TwoPointCrossover twoPointCrossover;
+
+        //Act
+        twoPointCrossover = new TwoPointCrossover();
+
+        //Assert
+        assertNotNull(twoPointCrossover);
+    }
+
+    @DisplayName("Probar el método crossover")
+    @Nested
+    public class CrossoverTest {
+        TwoPointCrossover twoPointCrossover;
+
+        @DisplayName("Se inicializa twoPointCrossover para cada test")
+        @BeforeEach
+        public void startUp() {
+            //Arrange
+            twoPointCrossover = new TwoPointCrossover();
+        }
+
+        @DisplayName("Cruzar con array padre 1 nulo lanza excepción")
+        @Test
+        public void crossover_NullParent1_ThrowsException() {
+            // Arrange
+            int[] parent1 = null;
+            int[] parent2 = {1, 3, 4};
+            
+            // Act, Assert
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                twoPointCrossover.crossover(parent1, parent2);
+            });
+        }
+        
+        @DisplayName("Cruzar array padre 2 nulo lanza excepción")
+        @Test
+        public void crossover_NullParent2_ThrowsException() {
+            // Arrange
+            int[] parent1 = {1, 3, 4};
+            int[] parent2 = null;
+            
+            // Act, Assert
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                twoPointCrossover.crossover(parent1, parent2);
+            });
+        }
+
+        @DisplayName("Cruzar array padre 1 con longitud menor o igual que 1 lanza excepción")
+        @Test
+        public void crossover_Parent2LengthLessThanOrEqualTo1_ThrowsException() {
+            // Arrange
+            int[] parent1 = {2};
+            int[] parent2 = {1, 3, 4};
+            
+            // Act, Assert
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                twoPointCrossover.crossover(parent1, parent2);
+            });
+        }
+        
+        @DisplayName("Cruzar array padre 1 con longitud diferente al array padre 2 lanza excepción")
+        @Test
+        public void crossover_Parent2LengthDifferentToParent2Length_ThrowsException() {
+            // Arrange
+            int[] parent1 = {2, 5, 3, 7};
+            int[] parent2 = {1, 3, 4};
+            
+            // Act, Assert
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                twoPointCrossover.crossover(parent1, parent2);
+            });
+        }
+
+        @DisplayName("Cruzar arrays válidos de longitud mínima (2) devuelve la misma longitud de offspring")
+        @Test
+        public void crossover_ValidParentsMininumLength_ReturnsSameLength() throws EvolutionaryAlgorithmException {
+            // Arrange
+            int[] parent1 = {1, 2};
+            int[] parent2 = {3, 4};
+
+            // Act
+            int[][] offspring = twoPointCrossover.crossover(parent1, parent2);
+
+            // Assert
+            assertEquals(parent1.length, offspring[0].length);
+            assertEquals(parent2.length, offspring[1].length);
+        }
+
+        @DisplayName("Cruzar arrays válidos de longitud mayor a la mínima devuelve la misma longitud de offspring")
+        @Test
+        public void crossover_ValidParentsLongerLengthThanMinimum_ReturnsSameLength() throws EvolutionaryAlgorithmException {
+            // Arrange
+            int[] parent1 = {1, 2, 3, 4, 5, 6};
+            int[] parent2 = {7, 8, 9, 10, 11, 12};
+
+            // Act
+            int[][] offspring = twoPointCrossover.crossover(parent1, parent2);
+
+            // Assert
+            assertEquals(parent1.length, offspring[0].length);
+            assertEquals(parent2.length, offspring[1].length);
+        }
+
+    }
 }
--- mps-lab4/src/test/java/org/mps/selection/TournamentSelectionTest.java
+++ mps-lab4/src/test/java/org/mps/selection/TournamentSelectionTest.java
@@ -1,10 +1,65 @@
 package org.mps.selection;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mps.EvolutionaryAlgorithmException;
+import org.mps.crossover.TwoPointCrossover;
+
 /**
  * @author Pablo Gámez Guerrero
  * @author Álvaro Gallardo Rubio
  */
 
+ //YOOOOOOOO
+
 public class TournamentSelectionTest {
-    
+    @DisplayName("Probar el constructor")
+    @Nested
+    public class TournamentSelectionConstructorTest {
+        @DisplayName("El constructor creado con un tamaño de torneo positivo se crea correctamente")
+        @Test
+        public void TournamentSelection_TournamentSizePositivo_SeCreaCorrectamente() throws EvolutionaryAlgorithmException {
+            //Arrange
+            TournamentSelection tournamentSelection;
+            int tournamentSize = 9;
+
+            //Act
+            tournamentSelection = new TournamentSelection(tournamentSize);
+
+            //Assert
+            assertNotNull(tournamentSelection);
+        }
+
+        @DisplayName("El constructor creado con un tamaño de torneo menor o igual a 0 lanza excepción")
+        @Test
+        public void TournamentSelection_TournamentSizeMenorIgualCero_LanzaExcepcion() throws EvolutionaryAlgorithmException {
+            // Arrange
+            int tournamentSize = 0;
+
+            //Act, Assert
+            assertThrows(EvolutionaryAlgorithmException.class, () -> {
+                new TournamentSelection(tournamentSize);
+            });
+        }
+    }
+
+    @DisplayName("Probar el método crossover")
+    @Nested
+    public class CrossoverTest {
+        TwoPointCrossover twoPointCrossover;
+
+        @DisplayName("Se inicializa twoPointCrossover para cada test")
+        @BeforeEach
+        public void startUp() {
+            twoPointCrossover = new TwoPointCrossover();
+        }
+
+        
+
+    }
 }
