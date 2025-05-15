d91785768dd2eb50bff14ac289bd417a96d7d12b
alvarogall <varosg@proton.me>
2025-05-15 11:06:05 +0200

    ControllersIT

---
+++ mps-lab6/src/test/java/com/uma/example/springuma/integration/MedicoControllerIT.java
@@ -0,0 +1,7 @@
+package com.uma.example.springuma.integration;
+
+import com.uma.example.springuma.integration.base.AbstractIntegration;
+
+public class MedicoControllerIT extends AbstractIntegration {
+    
+}
