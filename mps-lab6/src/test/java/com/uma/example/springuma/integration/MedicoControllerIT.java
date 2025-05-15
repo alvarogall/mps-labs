b1eaf0b50071b94d703e462ff94da31e9b91c042
alvarogall <varosg@proton.me>
2025-05-15 13:01:37 +0200

    Primer camino feliz completado

--- mps-lab6/src/test/java/com/uma/example/springuma/integration/MedicoControllerIT.java
+++ mps-lab6/src/test/java/com/uma/example/springuma/integration/MedicoControllerIT.java
@@ -1,7 +1,122 @@
 package com.uma.example.springuma.integration;
 
-import com.uma.example.springuma.integration.base.AbstractIntegration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uma.example.springuma.integration.base.AbstractIntegration;
+import com.uma.example.springuma.model.Medico;
+
+@DisplayName("Integración MedicoController")
 public class MedicoControllerIT extends AbstractIntegration {
-    
+    private static final String MEDICO_ENDPOINT = "/medico";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Medico createMedico(Long id, String dni, String nombre, String especialidad) {
+        Medico medico = new Medico(dni, nombre, especialidad);
+        medico.setId(id.intValue());
+        return medico;
+    }
+
+    @Test
+    @DisplayName("Guardar un médico y comprobar que se ha guardado")
+    public void saveMedico_NoExistingMedico_SavesMedicoCorrectly() throws Exception {
+        Medico medico = createMedico(1L, "dni", "nombre", "especialidad");
+
+        // Guardar médico
+        this.mockMvc.perform(post(MEDICO_ENDPOINT)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(medico)))
+            .andExpect(status().isCreated())
+            .andExpect(status().is2xxSuccessful());
+
+        // Comprobar que se ha guardado
+        this.mockMvc.perform(get(MEDICO_ENDPOINT + "/" + medico.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.id").value("1"))
+            .andExpect(jsonPath("$.dni").value("dni"))
+            .andExpect(jsonPath("$.nombre").value("nombre"))
+            .andExpect(jsonPath("$.especialidad").value("especialidad"));
+    }
+
+    @Test
+    @DisplayName("Actualizar un médico y comprobar que se ha actualizado")
+    public void updateMedico_ExistingMedico_UpdatesMedicoCorrectly() throws Exception {
+        Medico medico = createMedico(1L, "dni", "nombre", "especialidad");
+
+        // Guardar médico
+        this.mockMvc.perform(post(MEDICO_ENDPOINT)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(medico)))
+            .andExpect(status().isCreated());
+
+        Medico medicoActualizado = createMedico(1L, "dniActualizado", "nombreActualizado", "especialidadActualizada");
+
+        // Actualizar médico
+        this.mockMvc.perform(put(MEDICO_ENDPOINT)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(medicoActualizado)))
+            .andExpect(status().is2xxSuccessful());
+
+        // Comprobar que se ha actualizado
+        this.mockMvc.perform(get(MEDICO_ENDPOINT + "/" + medicoActualizado.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.id").value("1"))
+            .andExpect(jsonPath("$.dni").value("dniActualizado"))
+            .andExpect(jsonPath("$.nombre").value("nombreActualizado"))
+            .andExpect(jsonPath("$.especialidad").value("especialidadActualizada"));
+    }
+
+    @Test
+    @DisplayName("Obtener un médico")
+    public void getMedico_ExistingMedico_GetsMedicoCorrectly() throws Exception {
+        Medico medico = createMedico(1L, "dni", "nombre", "especialidad");
+
+        // Guardar médico
+        this.mockMvc.perform(post(MEDICO_ENDPOINT)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(medico)))
+            .andExpect(status().isCreated());
+
+        // Obtener médico y comprobar valores
+        this.mockMvc.perform(get(MEDICO_ENDPOINT + "/" + medico.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.id").value("1"))
+            .andExpect(jsonPath("$.dni").value("dni"))
+            .andExpect(jsonPath("$.nombre").value("nombre"))
+            .andExpect(jsonPath("$.especialidad").value("especialidad"));
+    }
+
+    @Test
+    @DisplayName("Eliminar un médico y comprobar que se ha eliminado")
+    public void deleteMedico_ExistingMedico_DeletesMedicoCorrectly() throws Exception {
+        Medico medico = createMedico(1L, "dni", "nombre", "especialidad");
+
+        // Guardar médico
+        this.mockMvc.perform(post(MEDICO_ENDPOINT)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(medico)))
+            .andExpect(status().isCreated());
+
+        // Eliminar médico
+        this.mockMvc.perform(delete(MEDICO_ENDPOINT + "/" + medico.getId()))
+            .andExpect(status().isOk());
+
+        // Comprobar que se ha eliminado
+        this.mockMvc.perform(get(MEDICO_ENDPOINT + "/" + medico.getId()))
+            .andExpect(status().is5xxServerError())
+            .andExpect(content().contentType("application/json"));
+    }
 }
