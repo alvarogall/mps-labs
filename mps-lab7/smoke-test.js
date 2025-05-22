a42b59666a69ce85b911a64da084b31ca3d824f1
alvarogall <varosg@proton.me>
2025-05-22 13:57:27 +0200

    Final

--- mps-lab7/smoke-test.js
+++ mps-lab7/smoke-test.js
@@ -1,26 +1,27 @@
 import http from "k6/http";
 import { check } from "k6";
 
-export const options  = {
+/**
+ * @type {import('k6/options').Options}
+ */
+export const options = {
     vus: 5,
     duration: '1m',
-
     thresholds: {
-        http_req_failed: ["rate==0"], // No debe haber fallos
-        http_req_duration: ['avg<100'], // Promedio < 100ms
+        http_req_failed: [
+            { threshold: 'rate==0', abortOnFail: true }
+        ],
+        http_req_duration: [
+            { threshold: 'avg<500', abortOnFail: true }
+        ],
     },
 };
 
 export default function () {
-
     const url = "http://localhost:8080/medico/1";
-
     const res = http.get(url);
 
-    console.log(res.body);
-
     check(res, {
-        "response code was 200": (res) => res.status == 200,
+        "response code was 200": (r) => r.status === 200,
     });
-
 }
