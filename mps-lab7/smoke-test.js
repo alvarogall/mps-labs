d598885ee845125fb3053abc5fae418efaf6dda0
Zemag_17 <tabletpablo26@gmail.com>
2025-05-22 12:22:26 +0200

    smoke-test.js hecho

---
+++ mps-lab7/smoke-test.js
@@ -0,0 +1,26 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export const options  = {
+    vus: 5,
+    duration: '1m',
+
+    thresholds: {
+        http_req_failed: ["rate==0"], // No debe haber fallos
+        http_req_duration: ['avg<100'], // Promedio < 100ms
+    },
+};
+
+export default function () {
+
+    const url = "http://localhost:8080/medico/1";
+
+    const res = http.get(url);
+
+    console.log(res.body);
+
+    check(res, {
+        "response code was 200": (res) => res.status == 200,
+    });
+
+}
